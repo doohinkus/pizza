27078791f81a6da72896c078d0c628793bac9df6
Rafael Perez <doohinkus@gmail.com>
2016-11-04 10:28:41 -0700

    complete first spec: user enters size and size is added to their order.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,51 @@
-function Pizza (){
-  this.toppings = [mushrooms, pineapple, pepperoni, ham, provolone, anchovies],
-  this.size = [small, medium, large, giant],
+function Customer (name, address) {
+  this.address = address,
+  this.bill = 0;
+  this.pizzas = [];
+}
+
+function Pizza (toppings, size){
+  this.toppings = toppings,
+  this.size = size,
   this.price = 0
 }
 //toppings
 var mushrooms = {name: "mushrooms", price: 1.00 };
-var pineapple = {name: "pineapple", price: 1.00 };
+var pineapple = {name: "pineapple", price: 1.25 };
 var pepperoni = {name: "pepperoni", price: 2.00 };
-var ham = {name: "ham", price: 2.00 };
+var ham = {name: "ham", price: 2.25 };
 var provolone = {name: "provolone", price: 1.50 };
 var anchovies = {name: "mushrooms", price: 1.00 };
-//sizes
-var small = {name: "small", price: 10.00};
-var medium = {name: "medium", price: 15.00};
-var large = {name: "large", price: 20.00};
-var giant = {name: "giant", price: 25.00};
 
-Pizza.prototype.addExtras = function(){
+var prices = [
+    {name: "small", price: 10.00},
+    {name: "medium",price: 15.00},
+    {name: "large", price: 20.00},
+    {name: "giant", price: 25.00}];
 
+
+Pizza.prototype.addExtras = function(size){
+  //sizes
+  var sizeSelection = "small";
+  prices.forEach (function (price){
+    if (price.name === size){
+      sizeSelection = price.price;
+    }
+
+  });
+  return this.price += parseFloat(sizeSelection);
 }
+
 
 $(document).ready(function (){
 
   $("#input").submit(function (event){
     event.preventDefault();
-    
+    var size = $("#size").val();
+    var pizza = new Pizza ("something", size);
+
+    console.log(pizza.size, " ", pizza.addExtras(size));
+    $("#output").text(pizza.size);
 
 
 
